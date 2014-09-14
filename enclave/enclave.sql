7a51ff37c72785181a545c3685b811eec911720f
karanlyons <karan@karanlyons.com>
2014-09-14 04:28:44 -0700

    Remove raise statements from Postgres SQL.
    These statements are executed regardless of logging level, and however
    minor it may be we really don’t want any unnecessary performance hits
    in functions that are going be run on many cells of data.
    
    We’re keeping the raised exception in enclave_verify_and_decrypt
    because that’s necessary to explain what’s going on, and additionally
    that function will only ever be used manually, and is there for
    convenience. The statement in our first anonymous function is also kept
    for explanatory purposes.

--- enclave/enclave.sql
+++ enclave/enclave.sql
@@ -7,8 +7,9 @@
 --             Null Flag (1)
 --             Data (?)
 -- Usage:
--- SELECT enclave_decrypt(enclave_encrypt(CAST('2014-07-23 08:41:52.004991-07' as timestamp), 'password'), 'password', CAST(null as timestamp));
 -- SELECT enclave_verify(enclave_encrypt(CAST('2014-07-23 08:41:52.004991-07' as timestamp), 'password'), 'password');
+-- SELECT enclave_decrypt(enclave_encrypt(CAST('2014-07-23 08:41:52.004991-07' as timestamp), 'password'), 'password', CAST(null as timestamp));
+-- SELECT enclave_verify_and_decrypt(enclave_encrypt(CAST('2014-07-23 08:41:52.004991-07' as timestamp), 'password'), 'password', CAST(null as timestamp));
 
 
 CREATE EXTENSION pgcrypto;
@@ -53,16 +54,6 @@ BEGIN
 	digest = encrypt_iv(decode(text_data, 'escape'), secret, iv, 'aes-cbc/pad:none'); -- text -> bytea and encrypt.
 	payload = iv || digest;
 	sig = hmac(payload, secret, 'sha256');
-	
-	RAISE DEBUG 'Encrypting...';
-	RAISE DEBUG 'Full:        %', sig || payload;
-	RAISE DEBUG 'Sig:         %', sig;
-	RAISE DEBUG 'Payload:     %', payload;
-	RAISE DEBUG 'IV:          %', iv;
-	RAISE DEBUG 'Digest:      %', digest;
-	RAISE DEBUG 'Padded Data: %', text_data;
-	RAISE DEBUG 'Pad Length:  %', pad_length;
-	RAISE DEBUG 'Data:        %', CAST(data as text);
 	RETURN sig || payload; -- Return sig(iv + payload) + IV + payload.
 END;
 $$ LANGUAGE plpgsql;
@@ -70,9 +61,6 @@ $$ LANGUAGE plpgsql;
 	
 CREATE OR REPLACE FUNCTION enclave_verify (digest bytea, secret bytea) RETURNS bool AS $$
 BEGIN
-	RAISE DEBUG 'Sig:         %', substring(digest, 0, 33);
-	RAISE DEBUG 'Payload:     %', substring(digest, 33);
-	RAISE DEBUG 'Sig:         %', hmac(substring(digest, 33), digest(secret, 'sha256'), 'sha256');
 	RETURN substring(digest, 0, 33) = hmac(substring(digest, 33), digest(secret, 'sha256'), 'sha256');
 END;
 $$ LANGUAGE plpgsql;
@@ -85,21 +73,13 @@ DECLARE
 	data text;
 	result ALIAS FOR $0;
 BEGIN
-	RAISE DEBUG 'Decrypting...';
-	RAISE DEBUG 'Full:        %', digest;
 	payload = substring(digest, 33);
-	RAISE DEBUG 'Payload:     %', payload;
 	
 	iv = substring(payload, 0, 17); -- IV is first 16 bytes.
 	digest = substring(payload, 17);
 	
-	RAISE DEBUG 'IV:          %', iv;
-	RAISE DEBUG 'Digest:      %', digest;
 	data = encode(decrypt_iv(digest, digest(secret, 'sha256'), iv, 'aes-cbc/pad:none'), 'escape'); -- bytea -> text.
-	RAISE DEBUG 'Padded Data: %', data;
-	RAISE DEBUG 'Pad Length:  %', CAST(substring(data, 0, 3) as integer);
 	data = substring(data, 3, length(data) - CAST(substring(data, 0, 3) as integer) - 2); -- First 2 characters are pad length.
-	RAISE DEBUG 'Data:        %', data;
 	
 	IF substring(data, 0, 2) = '1' THEN
 		RETURN null;
